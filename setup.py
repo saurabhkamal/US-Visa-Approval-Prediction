6a67f9531490ae88c68aa770cedb69c4da896497
Saurabh Kamal <saurabh.kamal1@gmail.com>
2024-05-05 22:40:09 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Saurabh Kamal",
+    author_email="saurabh.kamal1@gmail.com",
+    packages=find_packages()
+)
